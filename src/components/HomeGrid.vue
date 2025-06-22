97b2940052b3911b23b32ba5632b3afb6daf4b86
Damian <daba9@op.pl>
2025-06-22 15:14:29 +0200

    shadow

--- src/components/HomeGrid.vue
+++ src/components/HomeGrid.vue
@@ -226,7 +226,7 @@ section {
     position: relative;
     display: flex;
     flex-direction: column;
-    box-shadow: none;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);
 
     svg {
       opacity: 50%;
